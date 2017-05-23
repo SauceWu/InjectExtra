977ee1680a323f39f9aea6a41ecd0818accb8491
sauce <wuhaitian@weejinfu.com>
2017-05-23 15:29:16 +0800

    git add .

--- sample/app/src/main/java/me/example/sauce/databinding/FragmentTest.java
+++ sample/app/src/main/java/me/example/sauce/databinding/FragmentTest.java
@@ -1,46 +1,50 @@
-package me.example.sauce.databinding;
+package me.example.sauce.databinding
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 
-import com.example.sauce.databinding.R;
+import com.example.sauce.databinding.R
 
 
-import me.sauce.EventThread;
-import me.sauce.InjectExtra;
-import me.sauce.Subscribe;
-import me.sauce.rxBus.RxManager;
+import me.sauce.EventThread
+import me.sauce.InjectExtra
+import me.sauce.Subscribe
+import me.sauce.rxBus.RxBus
+import me.sauce.rxBus.RxManager
+import me.sauce.rxBus.UnSubscribe
 
 /**
  * Created by sauce on 2017/3/13.
  * Version 1.0.0
  */
-public class FragmentTest extends Fragment {
+class FragmentTest : Fragment() {
     @InjectExtra("data")
-    String data;
+    internal var data: String? = null
+    private var unSubscribe: UnSubscribe? = null
 
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.activity_main, container, false)
     }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        RxManager.init(this);
-        view.findViewById(R.id.bt).setOnClickListener(v -> me.sauce.rxBus.RxBus.getInstance().post(200));
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        unSubscribe = RxManager.init(this)
+        view!!.findViewById(R.id.bt).setOnClickListener { RxBus.getInstance().post(200) }
     }
 
-    @Subscribe(tag = 200, thread = EventThread.IO)
-    public void Event() {
-        Toast.makeText(getActivity(), "成功", Toast.LENGTH_SHORT).show();
+    @Subscribe(tag = 200, thread = EventThread.MAIN_THREAD)
+    fun Event() {
+        Toast.makeText(activity, "成功", Toast.LENGTH_SHORT).show()
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unSubscribe!!.unSubscribe()
     }
 }
